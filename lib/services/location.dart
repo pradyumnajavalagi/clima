cf82c5dac2e5d1948e3d1d1fcd51273c9682efcd
pradyumnajavalagi <122262501+pradyumnajavalagi@users.noreply.github.com>
2023-10-07 16:07:11 +0530

    Update location.dart

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,21 +1,4 @@
-// import 'package:geolocator/geolocator.dart';
-// class Location{
-//   late double latitude;
-//   late double longitude;
-//
-//   Future<void> getCurrentLocation() async{
-//     try{
-//       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-//       print(position);
-//       latitude = position.latitude;
-//       longitude = position.longitude;
-//     }catch(e){
-//       print(e);
-//     }
-//   }
-//
-// }
-//
+
 import 'package:geolocator/geolocator.dart';
 
 class Location {
